6118ddb469f892d4fba769b31883ce50a508002c
Peyton-Cysewski <pcysewski12@gmail.com>
2020-03-20 01:28:35 -0700

    More on cart.js

--- js/cart.js
+++ js/cart.js
@@ -25,16 +25,36 @@ function clearCart() {}
 function showCart() {
 
   // TODO: Find the table body
-
+  var tableEl = document.getElementsByTagName('tbody')[0];
+  console.log(tableEl);
   // TODO: Iterate over the items in the cart
   // TODO: Create a TR
   // TODO: Create a TD for the delete link, quantity,  and the item
   // TODO: Add the TR to the TBODY and each of the TD's to the TR
+  for (var i = 0; i < cart.items.length; i++) {
+    var rowEl = document.createElement('tr');
+    var itemCell = document.createElement('td');
+    var quantityCell = document.createElement('td');
+    var deleteCell = document.createElement('td');
+
+    // Making the delete link
+    var deleteEl = document.createElement('div');
+    // deleteEl.setAttribute('');
+
+    itemCell.textContent = cart.items[i].product;
+    quantityCell.textContent = cart.items[i].quantity;
+  
+    rowEl.appendChild(itemCell);
+    rowEl.appendChild(quantityCell);
+    rowEl.appendChild(deleteCell);
+
+    tableEl.appendChild(rowEl);
+  }
 
 }
 
 function removeItemFromCart(event) {
-
+  console.log('test');
   // TODO: When a delete link is clicked, use cart.removeItem to remove the correct item
   // TODO: Save the cart back to local storage
   // TODO: Re-draw the cart table
